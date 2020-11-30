3909ec8ffe333205c5f7d4454aaebd22647e22a5
MOUIGUINA MOHAMED <mohamedmouiguina7@gmail.com>
2020-11-30 14:29:55 +0100

    Add project files.

---
+++ NotionBase/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotionBase
+{
+    class Program
+    {
+        private static decimal price;
+        private static decimal percentage;
+        private static decimal finalePrice;
+
+        static void Main(string[] args)
+        {
+            //getting Price value
+            Console.WriteLine("Entre Price of item: ");
+            string price = Console.ReadLine();
+            //getting Percentage value
+            Console.WriteLine("Entre Percentage of item");
+            string percentage = Console.ReadLine();
+
+            //var finalePrice = decimal.Parse(price) + (decimal.Parse(price) * (decimal.Parse(percentage) / 100));
+
+            //Console.WriteLine(string.Format("Initial price: {0} dh, \r\n After Tax Price:{1} dh", price, finalePrice));
+            TaxCalculator(price, percentage);
+            GetMsg();
+
+        }
+
+        public static void TaxCalculator(string inputPrice, string taxRate)
+        {
+            price = decimal.Parse(inputPrice);
+            percentage = decimal.Parse(taxRate) / 100;
+            finalePrice = price + (price * percentage);
+
+        }
+
+        public static void GetMsg()
+        {
+            Console.WriteLine(string.Format("Initial price: {0} dh, \r\n  After Tax Price:{1} dg", price, finalePrice));
+        }
+
+
+
+
+    }
+}
