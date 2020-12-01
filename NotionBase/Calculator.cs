247541610ff3a3f1a1e59025755e587442b52d5f
MOUIGUINA MOHAMED <mohamedmouiguina7@gmail.com>
2020-12-01 20:53:47 +0100

    Changes to the calculator

--- NotionBase/Calculator.cs
+++ NotionBase/Calculator.cs
@@ -8,7 +8,8 @@ namespace NotionBase
 {
     class Calculator
     {
-
+        public static Decimal Num1, Num2;
+        public static byte quit = 0;
         public static void calculate()
         {
             /**
@@ -17,37 +18,62 @@ namespace NotionBase
              * based on the user choice apply the calculation
              */
 
-            Console.WriteLine("Calculator started : \r\n" +
+            while ( quit == 0)
+            {
+                Console.WriteLine("Calculator started : \r\n" +
                 "______________________\r\n" +
                 "choose an operation\r\n"
                 );
-            String operation = Console.ReadLine();
-            Console.WriteLine("First number: ");
-            Decimal Num1 = Convert.ToInt32(Console.ReadLine()); 
-            Console.WriteLine("Second number: ");
-            Decimal Num2 = Convert.ToInt32(Console.ReadLine());
+                String operation = Console.ReadLine();
+                TakeInput();
 
-            switch (operation)
-            {
-                case "+":
-                    Console.WriteLine(String.Format("The result of {0} {1} {2} = {3} ",Num1, operation, Num2, Num1+Num2));
-                    break;
-                case "-":
-                    Console.WriteLine(String.Format("The result of {0} {1} {2} = {3} ", Num1, operation, Num2, Num1 - Num2));
-                    break;
-                case "*":
-                    Console.WriteLine(String.Format("The result of {0} {1} {2} = {3} ", Num1, operation, Num2, Num1 * Num2));
-                    break;
-                case "/":
-                    Console.WriteLine(String.Format("The result of {0} {1} {2} = {3} ", Num1, operation, Num2, Num1 / Num2));
-                    break;
-                default:
-                    Console.WriteLine("there was an Error");
-                    //Error message
-                    break;
+                switch (operation)
+                {
+                    case "+":
+                        Console.WriteLine(String.Format("The result of {0} {1} {2} = {3} \r\n", Num1, operation, Num2, Num1 + Num2));
+                        break;
+                    case "-":
+                        Console.WriteLine(String.Format("The result of {0} {1} {2} = {3} \r\n", Num1, operation, Num2, Num1 - Num2));
+                        break;
+                    case "*":
+                        Console.WriteLine(String.Format("The result of {0} {1} {2} = {3} \r\n", Num1, operation, Num2, Num1 * Num2));
+                        break;
+                    case "/":
+                        Console.WriteLine(String.Format("The result of {0} {1} {2} = {3} \r\n", Num1, operation, Num2, Num1 / Num2));
+                        break;
+                    default:
+                        Console.WriteLine("Please enter a valid input");
+
+                        //Error message
+                        break;
+                }
+
+                Console.WriteLine("\r\n### Press Q to (quit) Or Press (Enter) to continue ###\r\n");
+                //var choice = Console.ReadKey();
+                if(Console.ReadKey().Key == ConsoleKey.Q)
+                {
+                    quit = 1;
+                }
+                else
+                {
+                    quit = 0;
+                }
+                 
+
+
             }
 
+            
 
+
+        }
+
+        public static void TakeInput()
+        {
+            Console.WriteLine("First number: ");
+            Num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Second number: ");
+            Num2 = Convert.ToInt32(Console.ReadLine());
         }
     }
 }
